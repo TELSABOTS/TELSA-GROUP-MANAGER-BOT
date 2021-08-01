18f27b45481b1f947e17844ed18072097c3d7c2b
HB <85717438+HBAMAL@users.noreply.github.com>
2021-08-01 17:22:08 +0530

    ꧁༒☬𝓗𝓑☬༒꧂

--- TELSA/modules/antiflood.py
+++ TELSA/modules/antiflood.py
@@ -6,10 +6,10 @@ from telegram.error import BadRequest
 from telegram.ext import Filters, MessageHandler, CommandHandler, run_async
 from telegram.utils.helpers import mention_html
 
-from tg_bot import dispatcher
-from tg_bot.modules.helper_funcs.chat_status import is_user_admin, user_admin, can_restrict
-from tg_bot.modules.log_channel import loggable
-from tg_bot.modules.sql import antiflood_sql as sql
+from TELSA import dispatcher
+from TELSA.modules.helper_funcs.chat_status import is_user_admin, user_admin, can_restrict
+from TELSA.modules.log_channel import loggable
+from TELSA.modules.sql import antiflood_sql as sql
 
 FLOOD_GROUP = 3
 
