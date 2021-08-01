e681cd0f35ae3b903fffb17eddd6a4a8cf1e2d93
HB <85717438+HBAMAL@users.noreply.github.com>
2021-08-01 17:30:44 +0530

    ꧁༒☬𝓗𝓑☬༒꧂

--- TELSA/modules/cust_filters.py
+++ TELSA/modules/cust_filters.py
@@ -8,14 +8,14 @@ from telegram.error import BadRequest
 from telegram.ext import CommandHandler, MessageHandler, DispatcherHandlerStop, run_async
 from telegram.utils.helpers import escape_markdown
 
-from tg_bot import dispatcher, LOGGER
-from tg_bot.modules.disable import DisableAbleCommandHandler
-from tg_bot.modules.helper_funcs.chat_status import user_admin
-from tg_bot.modules.helper_funcs.extraction import extract_text
-from tg_bot.modules.helper_funcs.filters import CustomFilters
-from tg_bot.modules.helper_funcs.misc import build_keyboard
-from tg_bot.modules.helper_funcs.string_handling import split_quotes, button_markdown_parser
-from tg_bot.modules.sql import cust_filters_sql as sql
+from TELSA import dispatcher, LOGGER
+from TELSA.modules.disable import DisableAbleCommandHandler
+from TELSA.modules.helper_funcs.chat_status import user_admin
+from TELSA.modules.helper_funcs.extraction import extract_text
+from TELSA.modules.helper_funcs.filters import CustomFilters
+from TELSA.modules.helper_funcs.misc import build_keyboard
+from TELSA.modules.helper_funcs.string_handling import split_quotes, button_markdown_parser
+from TELSA.modules.sql import cust_filters_sql as sql
 
 from tg_bot.modules.connection import connected
 
