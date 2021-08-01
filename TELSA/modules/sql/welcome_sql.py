2d7df1a93306ad784cfa060570f8cebb33d2129a
HB <85717438+HBAMAL@users.noreply.github.com>
2021-08-01 17:15:21 +0530

    ꧁༒☬𝓗𝓑☬༒꧂

--- TELSA/modules/sql/welcome_sql.py
+++ TELSA/modules/sql/welcome_sql.py
@@ -2,8 +2,8 @@ import threading
 
 from sqlalchemy import Column, String, Boolean, UnicodeText, Integer, BigInteger
 
-from tg_bot.modules.helper_funcs.msg_types import Types
-from tg_bot.modules.sql import SESSION, BASE
+from TELSA.modules.helper_funcs.msg_types import Types
+from TELSA.modules.sql import SESSION, BASE
 
 DEFAULT_WELCOME = "Hey {first}, how are you?"
 DEFAULT_GOODBYE = "Nice knowing ya!"
