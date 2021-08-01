b3bce8630773c43e255075c0e491bc49077397e9
HB <85717438+HBAMAL@users.noreply.github.com>
2021-08-01 17:45:29 +0530

    ꧁༒☬𝓗𝓑☬༒꧂

--- TELSA/modules/rss.py
+++ TELSA/modules/rss.py
@@ -5,9 +5,9 @@ from feedparser import parse
 from telegram import ParseMode, constants
 from telegram.ext import CommandHandler
 
-from tg_bot import dispatcher, updater
-from tg_bot.modules.helper_funcs.chat_status import user_admin
-from tg_bot.modules.sql import rss_sql as sql
+from TELSA import dispatcher, updater
+from TELSA.modules.helper_funcs.chat_status import user_admin
+from TELSA.modules.sql import rss_sql as sql
 
 
 def show_url(bot, update, args):
