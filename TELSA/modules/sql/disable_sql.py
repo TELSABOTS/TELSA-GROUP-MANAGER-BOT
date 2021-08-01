c6a914178171cd5b6442b7b0b8a069e7dea17a79
HB <85717438+HBAMAL@users.noreply.github.com>
2021-08-01 18:01:23 +0530

    ꧁༒☬𝓗𝓑☬༒꧂

--- TELSA/modules/sql/disable_sql.py
+++ TELSA/modules/sql/disable_sql.py
@@ -2,7 +2,7 @@ import threading
 
 from sqlalchemy import Column, String, UnicodeText, func, distinct
 
-from tg_bot.modules.sql import SESSION, BASE
+from RULES.modules.sql import SESSION, BASE
 
 
 class Disable(BASE):
