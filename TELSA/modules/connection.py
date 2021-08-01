31c5803169513417a36b571f25b052f36723f5e7
HB <85717438+HBAMAL@users.noreply.github.com>
2021-08-01 17:29:24 +0530

    ꧁༒☬𝓗𝓑☬༒꧂

--- TELSA/modules/connection.py
+++ TELSA/modules/connection.py
@@ -7,11 +7,11 @@ from telegram.ext import CommandHandler, Filters
 from telegram.ext.dispatcher import run_async
 from telegram.utils.helpers import mention_html
 
-import tg_bot.modules.sql.connection_sql as sql
-from tg_bot import dispatcher, LOGGER, SUDO_USERS
-from tg_bot.modules.helper_funcs.chat_status import bot_admin, user_admin, is_user_admin, can_restrict
-from tg_bot.modules.helper_funcs.extraction import extract_user, extract_user_and_text
-from tg_bot.modules.helper_funcs.string_handling import extract_time
+import TELSA.modules.sql.connection_sql as sql
+from TELSA import dispatcher, LOGGER, SUDO_USERS
+from TELSA.modules.helper_funcs.chat_status import bot_admin, user_admin, is_user_admin, can_restrict
+from TELSA.modules.helper_funcs.extraction import extract_user, extract_user_and_text
+from TELSA.modules.helper_funcs.string_handling import extract_time
 
 # from tg_bot.modules.translations.strings import tld
 
