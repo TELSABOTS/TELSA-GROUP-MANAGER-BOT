6a4ed2d0b7a487d1fb3059dc8cdae1d514197b41
HB <85717438+HBAMAL@users.noreply.github.com>
2021-08-01 17:54:21 +0530

    ꧁༒☬𝓗𝓑☬༒꧂

--- TELSA/modules/misc.py
+++ TELSA/modules/misc.py
@@ -10,11 +10,11 @@ from telegram import ParseMode
 from telegram.ext import CommandHandler, run_async, Filters
 from telegram.utils.helpers import escape_markdown, mention_html
 
-from tg_bot import dispatcher, OWNER_ID, SUDO_USERS, SUPPORT_USERS, WHITELIST_USERS, BAN_STICKER
-from tg_bot.__main__ import STATS, USER_INFO
-from tg_bot.modules.disable import DisableAbleCommandHandler
-from tg_bot.modules.helper_funcs.extraction import extract_user
-from tg_bot.modules.helper_funcs.filters import CustomFilters
+from TELSA import dispatcher, OWNER_ID, SUDO_USERS, SUPPORT_USERS, WHITELIST_USERS, BAN_STICKER
+from TELSA.__main__ import STATS, USER_INFO
+from TELSA.modules.disable import DisableAbleCommandHandler
+from TELSA.modules.helper_funcs.extraction import extract_user
+from TELSA.modules.helper_funcs.filters import CustomFilters
 
 RUN_STRINGS = (
     "Where do you think you're going?",
