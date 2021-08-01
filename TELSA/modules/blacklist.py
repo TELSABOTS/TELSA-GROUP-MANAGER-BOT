9e08ae5563cb58779ce6f55878cc156c0e886bf2
HB <85717438+HBAMAL@users.noreply.github.com>
2021-08-01 17:27:16 +0530

    ꧁༒☬𝓗𝓑☬༒꧂

--- TELSA/modules/blacklist.py
+++ TELSA/modules/blacklist.py
@@ -6,12 +6,12 @@ from telegram import Message, Chat, Update, Bot, ParseMode
 from telegram.error import BadRequest
 from telegram.ext import CommandHandler, MessageHandler, Filters, run_async
 
-import tg_bot.modules.sql.blacklist_sql as sql
-from tg_bot import dispatcher, LOGGER
-from tg_bot.modules.disable import DisableAbleCommandHandler
-from tg_bot.modules.helper_funcs.chat_status import user_admin, user_not_admin
-from tg_bot.modules.helper_funcs.extraction import extract_text
-from tg_bot.modules.helper_funcs.misc import split_message
+import TELSA.modules.sql.blacklist_sql as sql
+from TELSA import dispatcher, LOGGER
+from TELSA.modules.disable import DisableAbleCommandHandler
+from TELSA.modules.helper_funcs.chat_status import user_admin, user_not_admin
+from TELSA.modules.helper_funcs.extraction import extract_text
+from TELSA.modules.helper_funcs.misc import split_message
 
 BLACKLIST_GROUP = 11
 
