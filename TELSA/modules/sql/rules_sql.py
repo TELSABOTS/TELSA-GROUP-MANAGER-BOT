0dccb7af2dee8187d3dd5d34d921263a9d96d408
HB <85717438+HBAMAL@users.noreply.github.com>
2021-08-01 17:57:39 +0530

    ꧁༒☬𝓗𝓑☬༒꧂

--- TELSA/modules/sql/rules_sql.py
+++ TELSA/modules/sql/rules_sql.py
@@ -2,7 +2,7 @@ import threading
 
 from sqlalchemy import Column, String, UnicodeText, func, distinct
 
-from tg_bot.modules.sql import SESSION, BASE
+from TELSA.modules.sql import SESSION, BASE
 
 
 class Rules(BASE):
