132660f057ea630c2040c30f18a9dbfc1c846763
HB <85717438+HBAMAL@users.noreply.github.com>
2021-08-01 18:03:42 +0530

    ꧁༒☬𝓗𝓑☬༒꧂

--- TELSA/modules/sql/global_mutes_sql.py
+++ TELSA/modules/sql/global_mutes_sql.py
@@ -2,7 +2,7 @@ import threading
 
 from sqlalchemy import Column, UnicodeText, Integer, String, Boolean
 
-from tg_bot.modules.sql import BASE, SESSION
+from TELSA.modules.sql import BASE, SESSION
 
 
 class GloballyMutedUsers(BASE):
