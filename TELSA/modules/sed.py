daccc7c87f6c40b98ead2e002a6b78addca6addf
HB <85717438+HBAMAL@users.noreply.github.com>
2021-08-01 17:41:30 +0530

    ꧁༒☬𝓗𝓑☬༒꧂

--- TELSA/modules/sed.py
+++ TELSA/modules/sed.py
@@ -1,4 +1,3 @@
-
 import re
 import sre_constants
 
@@ -6,8 +5,8 @@ import telegram
 from telegram import Update, Bot
 from telegram.ext import run_async
 
-from tg_bot import dispatcher, LOGGER
-from tg_bot.modules.disable import DisableAbleRegexHandler
+from TELSA import dispatcher, LOGGER
+from TELSA.modules.disable import DisableAbleRegexHandler
 
 DELIMITERS = ("/", ":", "|", "_")
 
@@ -100,13 +99,6 @@ def sed(bot: Bot, update: Update):
 
 
 __help__ = """
- - s/<text1>/<text2>(/<flag>): Reply to a message with this to perform a sed operation on that message, replacing all \
-occurrences of 'text1' with 'text2'. Flags are optional, and currently include 'i' for ignore case, 'g' for global, \
-or nothing. Delimiters include `/`, `_`, `|`, and `:`. Text grouping is supported. The resulting message cannot be \
-larger than {}.
-*Reminder:* Sed uses some special characters to make matching easier, such as these: `+*.?\\`
-If you want to use these characters, make sure you escape them!
-eg: \\?.
 """.format(telegram.MAX_MESSAGE_LENGTH)
 
 __mod_name__ = "🌐Sed/Regex🌐"
