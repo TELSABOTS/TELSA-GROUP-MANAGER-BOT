402ce823e117501fbb8170ab17182f95ddaf5a3f
HB <85717438+HBAMAL@users.noreply.github.com>
2021-08-01 17:13:15 +0530

    ꧁༒☬𝓗𝓑☬༒꧂

--- TELSA/modules/helper_funcs/filters.py
+++ TELSA/modules/helper_funcs/filters.py
@@ -1,7 +1,7 @@
 from telegram import Message
 from telegram.ext import BaseFilter
 
-from tg_bot import SUPPORT_USERS, SUDO_USERS
+from TELSA import SUPPORT_USERS, SUDO_USERS
 
 
 class CustomFilters(object):
